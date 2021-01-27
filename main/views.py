75a02addf71fe4a2afd57cb31c61875812ab1530
abdimazhitovabilkhan <abdimazhitivabilkhan@gmail.com>
2021-01-27 17:15:08 +0600

    init

---
+++ main/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def homepage (request):
+    return HttpResponse("Абильхан Молодец!")
+
+
+def test (request):
+    return render (request, "test.html")
+
